2749e9820fc128b4e5d6d25fac90cbd03e582c85
FrankHong! <a13916392822@sina.com>
2024-08-04 02:03:29 -0500

    Fixed the methods in the class

--- RK4-solver.py
+++ RK4-solver.py
@@ -1,58 +1,65 @@
 import matplotlib.pyplot as plt
 
+params = {'prey_birthrate': 1,
+          'predator_eatrate': 0.01,
+          'predator_birthrate': 0.01,
+          'predator_deathrat': 0.5}
+
+
 def f_x(x, y, params):
-  a = params[0]
-  b = params[1]
+  a = params['prey_birthrate']
+  b = params['predator_eatrate']
   dx_dt = a * x - (b * x * y)
   return dx_dt
 
 
 def f_y(x, y, params):
-  c = params[2]
-  d = params[3]
+  c = params['predator_birthrate']
+  d = params['predator_deathrate']
   dy_dt = c * x * y - (d * y)
   return dy_dt
 
 
 class RK_4:
   
-  def __init__(self, approximation, h):
-      self.approximation = approximation
+  def __init__(self, deriv, h):
+      self.deriv = deriv
       self.step = h
       
-  def K_1(self, x, y):
-      K1 = self.approximate(x, y)
+  def K_1(self, x, y, *args):
+      K1 = self.deriv(x, y, *args)
       K1 = self.step * K1
       return K1
       
-  def K_2(self, x, y):
-      K1 = K_1(self, x, y)
-      x = x + (0.5 * h)
+  def K_2(self, x, y, *args):
+      K1 = self.K_1(x, y, *args)
+      x = x + (0.5 * self.step)
       y = y + (0.5 * K1)
-      K2 = self.approximate(x, y)
+      K2 = self.deriv(x, y, *args)
       K2 = self.step * K2
       return K2
   
-  def K_3(self, x, y):
-      K2 = K_2(self, x, y)
-      x = x + (0.5 * h)
+  def K_3(self, x, y, *args):
+      K2 = self.K_2(x, y, *args)
+      x = x + (0.5 * self.step)
       y = y + (0.5 * K2)
-      K3 = self.approximate(x, y)
+      K3 = self.deriv(x, y, *args)
       K3 = self.step * K3
       return K3
   
-  def K_4(self, x, y):
-      K3 = K_3(self, x, y)
-      x = x + h
+  def K_4(self, x, y, *args):
+      K3 = self.K_3(x, y, *args)
+      x = x + self.step
       y = y + K3
-      K4 = self.approximate(x, y)
+      K4 = self.deriv(x, y, *args)
       K4 = self.step * K4
       return K4
     
-  def operation(x, y):
-    val = (1/6) * (K_1(self, x, y) + 2 * K_2(self, x, y) + 2 * K_3(self, x, y) + K_4(self, x, y))
+  def operation(self, x, y, *args):
+    val = (1/6) * (self.K_1(x, y, *args) + 2 * self.K_2(x, y, *args) + 2 * self.K_3(x, y, *args) + self.K_4(x, y, *args))
     return val
       
-      
-x_deriv = RK4(self, f_x, h)
-y_deriv = RK4(self, f_y, h)
+h = 0.001
+x_deriv = RK_4(f_x, h)
+x = x_deriv.operation(2, 3, params)
+print(x)
